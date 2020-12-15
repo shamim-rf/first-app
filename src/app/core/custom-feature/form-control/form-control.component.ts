d210166d343dbceb8a819f698f1bee4f7e738757
shamim-rf <shamim.rf97@gmail.com>
2020-12-15 23:27:43 +0330

    #add reactive form and validation

--- src/app/core/custom-feature/form-control/form-control.component.ts
+++ src/app/core/custom-feature/form-control/form-control.component.ts
@@ -1,18 +1,25 @@
 import { TranslateService } from '@ngx-translate/core';
-import { NgControl, ValidationErrors } from '@angular/forms';
-import { Component, ContentChild, Input, OnInit } from '@angular/core';
+import { FormControl, FormControlName, FormGroup, NgControl, ValidationErrors } from '@angular/forms';
+import { AfterContentChecked, Component, ContentChild, Input, OnInit, ChangeDetectorRef } from '@angular/core';
 import { ErrorMessageType } from './../../../shared/types/error-message';
+import { RequiredFieldDirective } from './required.directive';
 
 @Component({
   selector: 'app-form-control',
   templateUrl: './form-control.component.html',
   styleUrls: ['./form-control.component.scss']
 })
-export class FormControlComponent implements OnInit {
+export class FormControlComponent implements OnInit, AfterContentChecked {
   // @Input() showMessages = true;
+  @Input('formControlName') set formControlName(value: FormGroup) {
+    value
+    debugger
+  }
   @Input() errorMessage: ErrorMessageType = new ErrorMessageType();
   @ContentChild(NgControl, { static: true }) private ngCtrl: NgControl;
-  constructor(private trans: TranslateService) { }
+  // @ContentChild(FormControl, { static: true }) private formTest: FormControl;
+  @ContentChild(RequiredFieldDirective, { static: true }) private requiredDirective: RequiredFieldDirective;
+  constructor(private tr: TranslateService, private cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
   }
@@ -20,41 +27,33 @@ export class FormControlComponent implements OnInit {
 
   get errorText() {
     let error;
+    // this.formTest;
     if (this.ngCtrl && this.ngCtrl.errors && (this.ngCtrl.touched)) {
-      error = this.localizeError(this.ngCtrl.errors);
+      error = this.generateErrors(this.ngCtrl.errors);
     }
     if (this.ngCtrl && this.ngCtrl.control.value) { this.ngCtrl.control.markAsTouched(); }
 
     return error;
   }
 
-  localizeError(err: ValidationErrors) {
+  generateErrors(err: ValidationErrors) {
     let text: string;
     if (err.required) {
-      this.errorMessage.required ? text = this.errorMessage.required : text = this.trans.instant('required');
-    } else if (err.min) {
-      this.errorMessage.min ? text = this.errorMessage.min :
-        text = this.trans.instant('validation-err-min').replace('{0}', err.min.requiredMin);
-    } else if (err.max) {
-      this.errorMessage.max ? text = this.errorMessage.max :
-        text = this.trans.instant('validation-err-max').replace('{0}', err.max.requiredMax);
-    } else if (err.minlength) {
-      this.errorMessage.minlength ? text = this.errorMessage.minlength :
-        text = this.trans.instant('validation-err-minlength').replace('{0}', err.minlength.requiredLength);
-    } else if (err.maxlength) {
-      this.errorMessage.maxlength ? text = this.errorMessage.maxlength :
-        text = this.trans.instant('validation-err-maxlength').replace('{0}', err.maxlength.requiredLength);
-    } else if (err.email) {
-      this.errorMessage.email ? text = this.errorMessage.email : text = this.trans.instant('validation-err-email');
-    } else if (err.nationalCodeValidator) {
-      this.errorMessage.nationalCodeValidator ? text = this.errorMessage.nationalCodeValidator :
-        text = this.trans.instant('validation-err-invalid-national-code');
-    } else if (err.equalWith) {
-      this.errorMessage.equalWith ? text = this.errorMessage.equalWith : text = err.equalWith.message;
-    } else {
-      this.errorMessage.customErrorMsg ? text = this.errorMessage.customErrorMsg :
-        text = this.trans.instant('validation-err-unknown');
+      this.errorMessage.required ? text = this.errorMessage.required : text = this.tr.instant('general.required');
     }
+    if (err.minlength) {
+      this.errorMessage.minlength ? text = this.errorMessage.minlength :
+        text = `${this.tr.instant('general.min')} ${err.minlength?.requiredLength} ${this.tr.instant('general.character')}`;
+    }
+    if (err.maxlength) {
+      this.errorMessage.maxlength ? text = this.errorMessage.maxlength :
+        text = `${this.tr.instant('general.max')} ${err.maxlength?.requiredLength} ${this.tr.instant('general.character')}`;
+    }
+
     return text;
+  }
+
+  ngAfterContentChecked() {
+    // this.cdr.detectChanges();
   }
 }
--- src/app/pages/user-mng/user-detail/user-personal-info/user-personal-info.component.ts
+++ src/app/pages/user-mng/user-detail/user-personal-info/user-personal-info.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SkipSelf } from '@angular/core';
 import { IEnum } from './../../../../shared/types/generalTypes';
 import { ErrorMessageType } from './../../../../shared/types/error-message';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { RxwebValidators } from '@rxweb/reactive-form-validators';
 @Component({
   selector: 'app-user-personal-info',
   templateUrl: './user-personal-info.component.html',
-  styleUrls: ['./user-personal-info.component.scss']
+  styleUrls: ['./user-personal-info.component.scss'],
 })
 export class UserPersonalInfoComponent implements OnInit {
+  @Input() modelGroupName: string;
   form: FormGroup;
   EducationItems: IEnum[] = [
+    { text: 'general.defualt-text', value: '' },
     { text: 'user.cycle', value: 'C' },
     { text: 'user.diploma', value: 'D' },
     { text: 'user.associate', value: 'A' },
@@ -18,22 +21,32 @@ export class UserPersonalInfoComponent implements OnInit {
     { text: 'user.doctorate', value: 'DO' }
   ];
   genderItems: IEnum[] = [
+    { text: 'general.defualt-text', value: '' },
     { text: 'user.male', value: 'Male' },
     { text: 'user.female', value: 'Female' },
     { text: 'user.nothing', value: 'Nothing' },
   ];
   err: ErrorMessageType = new ErrorMessageType();
   constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-      firstName: [null, new FormControl([Validators.required, Validators.maxLength(10)])],
-      lastName: [null, new FormControl([Validators.required, Validators.maxLength(10)])],
-      gender: [null, new FormControl()],
-      employeeDate: [null, new FormControl('')]
-    });
   }
 
   ngOnInit(): void {
-    this.err.maxlength = 'hello';
+    this.form = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: [null, Validators.compose([Validators.required, Validators.minLength(3)])],
+      gender: [null],
+      educationLevel: [null],
+      employeeDate: [null, new FormControl('')],
+      birthDate: [null, new FormControl('')],
+      address: this.fb.group({
+        tel: [null , [RxwebValidators.maxDate]]
+      })
+    });
   }
+
+  // public errorHandling = (control: string, error: string) => {
+
+  //   return this.form.controls[control].hasError(error);
+  // }
 
 }
